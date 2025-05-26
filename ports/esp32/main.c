d9a3cc0d68c5ea13cd753e0b83cc92d2356f5ef9
Trent Walraven <trwbox@gmail.com>
2025-05-26 06:35:29 -0500

    esp32/network_wlan: Add scan configurations and non-blocking.
    Adding keyword arguments to the scan function to change the configuration
    options. This includes scanning by ssid, bssid, hidden networks, scan
    type, various scan timings, the channel, and if using ESP-IDF greater
    than  5.3.0 scanning a set of channels. This also add the ability to use
    the ESP-IDF's non-blocking scan, using a user provided callback. This
    callback will get called after the scan is complete, and takes in two
    arguments, the first being the interface that completed the scan, and
    the second being a tuple containing two objects. The first object is
    the bool value of the status of the scan, and the second is the list of
    networks in the standard format. If the scan method is called in a
    non-blocking manner, it will return None instead of a list of networks.
    The default keyword parameters result in the same scan being completed
    that would occur with the previously empty configuration. This also
    adds a method to check if a scan is currently in progress that returns
    a bool.
    
    Signed-off-by: Trent Walraven <trwbox@gmail.com>

--- ports/esp32/main.c
+++ ports/esp32/main.c
@@ -199,6 +199,7 @@ soft_reset_exit:
     // TODO: machine_rmt_deinit_all();
     machine_pins_deinit();
     machine_deinit();
+    network_wlan_deinit();
     #if MICROPY_PY_SOCKET_EVENTS
     socket_events_deinit();
     #endif
